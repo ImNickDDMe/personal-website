371266df25cac05c88294f001d4b1cdbbad76826
Nikolaos Papanastasis <nickpapanastasis9@gmail.com>
2024-05-01 14:40:43 +0300

    Initial Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Content } from 'carbon-components-svelte';
+	import 'carbon-components-svelte/css/g10.css';
+</script>
+
+<svelte:head>
+	<title>Nikolaos Papanastasis</title>
+</svelte:head>
+
+<Content>
+	<slot></slot>
+</Content>
