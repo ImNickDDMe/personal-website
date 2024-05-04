b9af9c1c4fcb0d4cc9d4bb80891976f1f2184222
Nikolaos Papanastasis <nickpapanastasis9@gmail.com>
2024-05-04 20:49:02 +0300

    Remove Carbon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Header from '$lib/components/Header.svelte';
 	import { Content } from 'carbon-components-svelte';
 	import 'carbon-components-svelte/css/g10.css';
 </script>
@@ -7,9 +6,3 @@
 <svelte:head>
 	<title>Nikolaos Papanastasis</title>
 </svelte:head>
-
-<Header />
-
-<Content>
-	<slot></slot>
-</Content>
